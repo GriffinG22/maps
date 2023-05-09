da2569bf4ab65048311fc6844498c44307b2cc5f
Griffin Gradke <griffin.gradke@gmail.com>
2023-05-09 14:12:09 -0500

    private map reusable addMarker method + inteface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,13 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -12,5 +22,47 @@ export class CustomMap {
         },
       }
     );
+  }
+
+  //Bad no need to repeat code
+  /* addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  } */
+
+  //Bad because it isnt scalable - will work here but becomes an issue when adding many other class types
+  /* addMarker(mappableObj: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappableObj.location.lat,
+        lng: mappableObj.location.lng,
+      },
+    });
+  } */
+
+  addMarker(mappableObj: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappableObj.location.lat,
+        lng: mappableObj.location.lng,
+      },
+    });
   }
 }
